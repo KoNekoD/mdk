f8e37448c9d541137ae3f2b2ef24f6c653de2c80
Neko <kawaii_nekochan@tutanota.com>
2022-10-12 10:41:14 +0300

    Release

---
+++ calc.php
@@ -0,0 +1,28 @@
+<?php
+function solveCalc(string $expressionStr): float
+{
+	$dictionaryOperators = ['+', '-', '*', '/'];
+	$dictionaryNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+	$expressionArray = str_split($expressionStr);
+
+	foreach ($expressionArray as $char)
+	{
+		if (!in_array($char, $dictionaryOperators) && !in_array($char, $dictionaryNumbers) || str_contains($expressionStr, '/0'))
+		{
+			throw new Exception('Invalid expression');
+		}
+
+	}
+
+	return eval('return '.$expressionStr.';');
+}
+
+try
+{
+	echo solveCalc('1+1*5/2/0');	
+} 
+catch (Exception $e)
+{
+	echo $e->getMessage();
+}
---
+++ time.php
@@ -0,0 +1,40 @@
+<?php
+
+function sumTime(string $firstTime, string $secondTime): string
+{
+	$dictionaryOperators = [':'];
+	$dictionaryNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+	$expressionArray = str_split($firstTime.$secondTime);
+
+	foreach ($expressionArray as $char)
+	{
+		if (!in_array($char, $dictionaryOperators) && !in_array($char, $dictionaryNumbers))
+		{
+			throw new Exception('Invalid expression');
+		}
+
+	}
+
+	sscanf($firstTime, '%d:%d:%d', $firstTimeHours, $firstTimeMinutes, $firstTimeSeconds);
+	sscanf($secondTime, '%d:%d:%d', $secondTimeHours, $secondTimeMinutes, $secondTimeSeconds);
+
+	$firstTimeMinutes += $firstTimeHours * 60;
+	$secondTimeMinutes += $secondTimeHours * 60;
+
+	$firstTimeSeconds += $firstTimeMinutes * 60;
+	$secondTimeSeconds += $secondTimeMinutes * 60;
+
+	$totalSeconds = $firstTimeSeconds + $secondTimeSeconds;
+
+	return gmdate('H:i:s', $totalSeconds);
+}
+
+try
+{
+	echo sumTime('11:22:00', '01:03:00');
+}
+catch (Exception $e)
+{
+	echo $e->getMessage();
+}
